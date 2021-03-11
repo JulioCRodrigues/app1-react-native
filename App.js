13e363f8b0f41eddbacbcc0d3f970321ce8d636a
JulioCRodrigues <julio_cesar.jcr@hotmail.com>
2021-03-10 21:39:10 -0300

    Subindo o projeto

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {View,Text} from 'react-native';
+
+
+
+export default class App1 extends Component{
+
+  render(){
+    return(
+      <View>
+          <Text>Olá mundo</Text>
+      </View>
+    );
+  }
+
+} 
+
+/*
+export default function app1() {
+  return (
+    <View>
+      <View>
+        <Text>Julio Rodrigues</Text>
+        <Text>Desenvolvedor Mobile</Text>
+      <View>  
+      </View>
+          <Text>Testando a aula</Text>
+          <Text>Terei sucesso</Text>
+      </View>
+    </View>  
+  );
+};
+
+
+*/
