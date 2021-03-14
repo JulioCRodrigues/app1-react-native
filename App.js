722ed4a4e80f16974f5018312b5799096e17e7e8
JulioCRodrigues <julio_cesar.jcr@hotmail.com>
2021-03-14 17:02:55 -0300

    Layouts com propriedades ajustaveis FLEX

--- App.js
+++ App.js
@@ -1,31 +1,14 @@
 //import React, { Component } from 'react';
 import React from 'react';
 import {StyleSheet, View,Text} from 'react-native';
-import C1 from './componentes/comp1'
+import Caixa from './componentes/caixas'
 import Estilos from './estilos/estilos'
 
 
-/*
-export default class App1 extends Component{
-
-  render(){
-    return(
-      <View>
-          <Text>Olá mundo das Natashas</Text>
-      </View>
-    );
-  }
-
-} 
-
-*/
 export default function app1() {
   return(
     <View style={Estilos.conteiner}>
-      <C1 curso="React Native"/>
-      <C1 curso="Java"/>
-      <C1 curso="Python"/>
-      <C1 curso="Dart"/>
+      <Caixa/>
       <Text style={Estilos.textoTitulo}> Julio Rodrigues - Cursos</Text>
     </View>
   );
---
+++ componentes/caixas.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View} from 'react-native';
+
+
+export default function(props){
+    return(
+        
+        <View style={{width:'100%', height:300,flexDirection: 'column', justifyContent:'center', alignItems:'stretch'}}>
+            <View style={{flexGrow: 1, backgroundColor:'#00F'}}></View>
+            <View style={{flexGrow: 2, backgroundColor:'#00a'}}></View>
+            <View style={{flexGrow: 3, backgroundColor:'#005'}}></View>
+            <View style={{flexGrow: 3, backgroundColor:'#f00'}}></View>
+            <View style={{flexGrow: 2, backgroundColor:'#a00'}}></View>
+            <View style={{flexGrow: 1, backgroundColor:'#500'}}></View>
+        </View>
+    )
+}
