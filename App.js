fb8b9e6f19b01ddc42f912938e8f52ab4ae54447
JulioCRodrigues <julio_cesar.jcr@hotmail.com>
2021-03-17 20:29:02 -0300

    Imagem de fundo

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
 //import React, { Component } from 'react';
 import React from 'react';
-import {StyleSheet, View,Text, Image, Button, Alert} from 'react-native';
+import {StyleSheet, View,Text, ImageBackground} from 'react-native';
 import Caixa from './componentes/caixas'
 import Estilos from './estilos/estilos'
 import ListaPlana from './componentes/flatlist'
 
+const imgBack1 = './assets/bg1.jpg'
+const imgBack2 = './assets/bg2.jpg'
+
 export default function app1() {
   return(
     <View style={Estilos.conteiner}>
-      <Text> Julio Rodrigues - Cursos</Text>
-      <Text> Curso de React Native</Text>
-      <ListaPlana/>
+     <ImageBackground
+     
+     source={require(imgBack1)}
+     style={() => {}}
+     
+     >
+          <Text> Julio Rodrigues - Cursos</Text>
+          <Text> Curso de React Native</Text>
+          <ListaPlana/>
+     </ImageBackground>
     </View>
   );
 };
@@ -19,5 +29,6 @@ const estilos = StyleSheet.create({
   logo:{
     width: 250,
     resizeMode: 'contain' // ajusta a imagem em relação ao width
-  }
+  }, 
+
 })
