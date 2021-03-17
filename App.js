e5dce92403c32e14238cae7bc53110bb1b642f6b
JulioCRodrigues <julio_cesar.jcr@hotmail.com>
2021-03-17 20:46:49 -0300

    UseState e suas funcionalidades

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 //import React, { Component } from 'react';
-import React from 'react';
-import {StyleSheet, View,Text, ImageBackground} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View,Text,Button} from 'react-native';
 import Caixa from './componentes/caixas'
 import Estilos from './estilos/estilos'
 import ListaPlana from './componentes/flatlist'
@@ -9,18 +9,24 @@ const imgBack1 = './assets/bg1.jpg'
 const imgBack2 = './assets/bg2.jpg'
 
 export default function app1() {
+
+const [ligado, setLigado] = useState(true)
+
+
+
   return(
     <View style={Estilos.conteiner}>
-     <ImageBackground
-     
-     source={require(imgBack1)}
-     style={() => {}}
-     
-     >
+      <Button
+        title={ligado?"Desligar":"Ligar"}
+        onPress={() =>setLigado(!ligado)}
+      />
+      {ligado?
+        <View>
           <Text> Julio Rodrigues - Cursos</Text>
           <Text> Curso de React Native</Text>
-          <ListaPlana/>
-     </ImageBackground>
+        </View>
+        : <Text>Falha ao carregar</Text>
+      }
     </View>
   );
 };
