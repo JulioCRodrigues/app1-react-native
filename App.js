8dd4a206400fab04b7748940d0b08e4ca78263f7
JulioCRodrigues <julio_cesar.jcr@hotmail.com>
2021-03-17 19:43:19 -0300

    Manipulando imagens e funções

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
 //import React, { Component } from 'react';
 import React from 'react';
-import {StyleSheet, View,Text} from 'react-native';
+import {StyleSheet, View,Text, Image, Button, Alert} from 'react-native';
 import Caixa from './componentes/caixas'
 import Estilos from './estilos/estilos'
 
+const dobro = n =>{
+  return n * 2;
+}
+
+const soma = (n1, n2) =>{
+  return n1 + n2;
+}
+
+
+const fexibir = (vp1) =>{
+  if(vp1 == true){
+    return <Text>Curso de React Native</Text>
+  } else{
+    return <Text>- - -</Text>
+  }
+}
 
 export default function app1() {
   return(
     <View style={Estilos.conteiner}>
-      <Caixa/>
+      <Text>{dobro(6)}</Text>
+      <Text>{soma(5,5)}</Text>
+        <Image
+          style={estilos.logo}
+          source={require('./assets/logocanal.png')}
+        />
       <Text style={Estilos.textoTitulo}> Julio Rodrigues - Cursos</Text>
+      {fexibir(false)}
     </View>
   );
 };
+
+const estilos = StyleSheet.create({
+  logo:{
+    width: 250,
+    resizeMode: 'contain' // ajusta a imagem em relação ao width
+  }
+})
--- componentes/caixas.js
+++ componentes/caixas.js
@@ -7,11 +7,8 @@ export default function(props){
         
         <View style={{width:'100%', height:300,flexDirection: 'column', justifyContent:'center', alignItems:'stretch'}}>
             <View style={{flexGrow: 1, backgroundColor:'#00F'}}></View>
-            <View style={{flexGrow: 2, backgroundColor:'#00a'}}></View>
-            <View style={{flexGrow: 3, backgroundColor:'#005'}}></View>
-            <View style={{flexGrow: 3, backgroundColor:'#f00'}}></View>
-            <View style={{flexGrow: 2, backgroundColor:'#a00'}}></View>
-            <View style={{flexGrow: 1, backgroundColor:'#500'}}></View>
+            <View style={{flexGrow: 1, backgroundColor:'#00a'}}></View>
+            <View style={{flexGrow: 1, backgroundColor:'#005'}}></View>
         </View>
     )
 }
